e999aa3f06fdc9a86f35e5e38a2814e6f1670aa7
Mehmetcan Vardar <45522624+mcanv@users.noreply.github.com>
2021-03-28 16:03:34 +0300

    Update index.js
    Numara kontrolü düzenlendi

--- js/index.js
+++ js/index.js
@@ -1,15 +1,29 @@
-var input = prompt("Giriş yapın!");
+var input = prompt("Lütfen bir metin giriniz");
 var charsArray = input.split("");
 
 function checkHasNumber(array) {
+    var numbers = [
+        '0',
+        '1',
+        '2',
+        '3',
+        '4',
+        '5',
+        '6',
+        '7',
+        '8',
+        '9',
+    ];
     for(var i = 0; i < array.length; i++) {
-        if(array[i] / 1 == array[i]) {
-            return true;
+        for(var j = 0; j < numbers.length; j++) {
+            if(numbers[j] === array[i]) {
+                return true;
+            }
         }
     }
 }
 
-function checkSpecial(array) {
+function checkSpecialChars(array) {
     var specialChars = [
         '*',
         '#',
@@ -42,7 +56,7 @@ function checkSpecial(array) {
     ];
     for(var i = 0; i < array.length; i++) {
         for(var j = 0; j < specialChars.length; j++) {
-            if(specialChars[j] == array[i]) {
+            if(specialChars[j] === array[i]) {
                 return true;
             }
         }
@@ -61,19 +75,19 @@ function showAlert(msg) {
 
 if(checkHasNumber(charsArray)) {
     showAlert("Lütfen içerisinde numara olmayan bir değer girin!");
-} else if(checkSpecial(charsArray)) {
+} else if(checkSpecialChars(charsArray)) {
     showAlert("Lütfen girdiğiniz kelime içerisinde özel karakter kullanmayınız!")
 } else if(checkMinMax(1, 9, input)) {
     showAlert("Lütfen minimum 1 maximum 9 karaktere sahip değer girin!")
 } else {
-    document.write("<div class='title'><h3>Girilen değer: "+ input + "</h3>"+"</div>");
-    document.write("<div class='title2'><h3>Oluşturulan kelimeler</h3></div>")
+    document.write(`<div class="title"><h3>${input}</h3></div>`);
+    document.write('<div class="title2"><h3>Oluşturulan kelimeler</h3></div>')
     for (var i = 0; i < charsArray.length; i++) {
         var randomizedString = "";
         for (var sayac = 0; sayac <= i; sayac++) {
             randomizedString += input[Math.floor(Math.random() * input.length)];
         }
         var numberOfChar = i + 1;
-        document.write("<li class='list'><span class='indicator'>" + numberOfChar + " karakterli:</span>" + randomizedString +"</li>");
+        document.write(`<li class="list"><span class="indicator">${numberOfChar} karakterli</span><span class="list-text">${randomizedString}</span></li>`)
     }
 }
